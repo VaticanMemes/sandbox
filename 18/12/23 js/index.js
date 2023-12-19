17ccc48947581165cab08eb432c0e791a61b184a
Vatican Memes <feehanharry@gmail.com>
2023-12-19 22:16:04 +1100

    made a good website skeleton

--- 18/12/23 js/index.js
+++ 18/12/23 js/index.js
@@ -1,54 +1,30 @@
-const content = document.getElementById("content")
+const content = document.getElementById("content");
 
-initialiseWebsite()
+initialiseWebsite();
+loadHome();
 
 function initialiseWebsite () {
-    createHeader()
-    // createBody()
-    // createFooter()
+    content.appendChild(createHeader());
+    content.appendChild(createBody());
+    content.appendChild(createFooter());
 }
 
 function createHeader () {
-    /*
-    const header = document.createElement("div")
-    const navHeader = document.createElement("nav")
-    navHeader.className = "navbar navbar-expand-lg bg-body-tertiary"
-    header.appendChild(navHeader)
-    const navContainer = document.createElement("div")
-    navContainer.className = "container-fluid"
-    navHeader.appendChild(navContainer)
-    const navA = document.createElement("a")
-    navA.className = "navbar-brand"
-    navA.innerText = "To do list"
-    navContainer.appendChild(navA)
-    const navButton1 = document.createElement("button")
-    navButton1.className = "navbar-toggler"
-    navButton1.setAttribute("type", "button")
-    navButton1.setAttribute("data-bs-toggle", "collapse")
-    navButton1.setAttribute("aria-expanded", "false")
-    navButton1.setAttribute("aria-label", "Toggle navigation")
-    navHeader.appendChild(navButton1)
-    const navButton1Span = document.createElement("span")
-    navButton1Span.className = "navbar-toggler-icon"
-    navContainer.appendChild(navButton1)
-    console.log(header)
-    content.appendChild(header)
-    */
     // Lmao just used the html to dom converter thingy
     var node_1 = document.createElement('NAV');
     node_1.setAttribute('class', 'navbar navbar-expand-lg bg-body-tertiary');
     
     var node_2 = document.createElement('DIV');
-    node_2.setAttribute('class', 'container-fluid');
+    node_2.setAttribute('class', 'container-md');
     node_1.appendChild(node_2);
     
     var node_3 = document.createElement('A');
     node_3.setAttribute('class', 'navbar-brand');
-    node_3.setAttribute('href', '#');
     node_2.appendChild(node_3);
     
-    var node_4 = document.createTextNode((new String("Navbar")));
-    node_3.appendChild(node_4);
+    // var node_4 = document.createTextNode(("To do &#128466;"));
+    node_3.innerHTML = "To do &#9999;"
+    // node_3.appendChild(node_4);
     
     var node_5 = document.createElement('BUTTON');
     node_5.setAttribute('class', 'navbar-toggler');
@@ -77,125 +53,102 @@ function createHeader () {
     node_9.setAttribute('class', 'nav-item');
     node_8.appendChild(node_9);
     
-    var node_10 = document.createElement('A');
-    node_10.setAttribute('class', 'nav-link active');
+    var node_10 = document.createElement('BUTTON');
+    node_10.setAttribute('class', 'button-nav nav-link');
     node_10.setAttribute('aria-current', 'page');
-    node_10.setAttribute('href', '#');
+    node_10.addEventListener("click", (e) => {
+      if (e.target.classList.contains("active")) return;
+      setActiveButton(node_10);
+      loadHome();
+    });
     node_9.appendChild(node_10);
     
-    var node_11 = document.createTextNode((new String("Home")));
+    var node_11 = document.createTextNode((new String("Create")));
     node_10.appendChild(node_11);
-    
+
     var node_12 = document.createElement('LI');
     node_12.setAttribute('class', 'nav-item');
     node_8.appendChild(node_12);
     
-    var node_13 = document.createElement('A');
-    node_13.setAttribute('class', 'nav-link');
-    node_13.setAttribute('href', '#');
+    var node_13 = document.createElement('BUTTON');
+    node_13.setAttribute('class', 'button-nav nav-link');
+    node_13.setAttribute('aria-current', 'page');
+    node_13.addEventListener("click", (e) => {
+      if (e.target.classList.contains("active")) return;
+      setActiveButton(node_13);
+      loadCreate();
+    });
     node_12.appendChild(node_13);
     
-    var node_14 = document.createTextNode((new String("Link")));
+    var node_14 = document.createTextNode((new String("Home")));
     node_13.appendChild(node_14);
-    
-    var node_15 = document.createElement('LI');
-    node_15.setAttribute('class', 'nav-item dropdown');
-    node_8.appendChild(node_15);
-    
-    var node_16 = document.createElement('A');
-    node_16.setAttribute('class', 'nav-link dropdown-toggle');
-    node_16.setAttribute('href', '#');
-    node_16.setAttribute('role', 'button');
-    node_16.setAttribute('data-bs-toggle', 'dropdown');
-    node_16.setAttribute('aria-expanded', 'false');
-    node_15.appendChild(node_16);
-    
-    var node_17 = document.createTextNode((new String("Dropdown")));
-    node_16.appendChild(node_17);
-    
-    var node_18 = document.createElement('UL');
-    node_18.setAttribute('class', 'dropdown-menu');
-    node_15.appendChild(node_18);
-    
-    var node_19 = document.createElement('LI');
-    node_18.appendChild(node_19);
-    
-    var node_20 = document.createElement('A');
-    node_20.setAttribute('class', 'dropdown-item');
-    node_20.setAttribute('href', '#');
-    node_19.appendChild(node_20);
-    
-    var node_21 = document.createTextNode((new String("Action")));
-    node_20.appendChild(node_21);
-    
-    var node_22 = document.createElement('LI');
-    node_18.appendChild(node_22);
-    
-    var node_23 = document.createElement('A');
-    node_23.setAttribute('class', 'dropdown-item');
-    node_23.setAttribute('href', '#');
-    node_22.appendChild(node_23);
-    
-    var node_24 = document.createTextNode((new String("Another action")));
-    node_23.appendChild(node_24);
-    
-    var node_25 = document.createElement('LI');
-    node_18.appendChild(node_25);
-    
-    var node_26 = document.createElement('HR');
-    node_26.setAttribute('class', 'dropdown-divider');
-    node_25.appendChild(node_26);
-    
-    var node_27 = document.createElement('LI');
-    node_18.appendChild(node_27);
-    
-    var node_28 = document.createElement('A');
-    node_28.setAttribute('class', 'dropdown-item');
-    node_28.setAttribute('href', '#');
-    node_27.appendChild(node_28);
-    
-    var node_29 = document.createTextNode((new String("Something else here")));
-    node_28.appendChild(node_29);
-    
-    var node_30 = document.createElement('LI');
-    node_30.setAttribute('class', 'nav-item');
-    node_8.appendChild(node_30);
-    
-    var node_31 = document.createElement('A');
-    node_31.setAttribute('class', 'nav-link disabled');
-    node_31.setAttribute('aria-disabled', 'true');
-    node_30.appendChild(node_31);
-    
-    var node_32 = document.createTextNode((new String("Disabled")));
-    node_31.appendChild(node_32);
-    
-    var node_33 = document.createElement('FORM');
-    node_33.setAttribute('class', 'd-flex');
-    node_33.setAttribute('role', 'search');
-    node_7.appendChild(node_33);
-    
-    var node_34 = document.createElement('INPUT');
-    node_34.setAttribute('class', 'form-control me-2');
-    node_34.setAttribute('type', 'search');
-    node_34.setAttribute('placeholder', 'Search');
-    node_34.setAttribute('aria-label', 'Search');
-    node_33.appendChild(node_34);
-    
-    var node_35 = document.createElement('BUTTON');
-    node_35.setAttribute('class', 'btn btn-outline-success');
-    node_35.setAttribute('type', 'submit');
-    node_33.appendChild(node_35);
-    
-    var node_36 = document.createTextNode((new String("Search")));
-    node_35.appendChild(node_36);
 
-    content.appendChild(node_1)
+    return node_1;
+}
+
+function createBody() {
+    var node_1 = document.createElement("main");
+    node_1.classList.add("main");
+    node_1.setAttribute("id", "main");
+
+    return node_1;
+}
+
+function createFooter () {
+    // Lmao just used the html to dom converter thingy
+    var node_2 = document.createElement('DIV');
+    node_2.setAttribute('class', 'footer container-md');
+    
+    var node_3 = document.createElement('P');
+    node_2.appendChild(node_3);
+    
+    var node_4 = document.createElement('A');
+    node_4.setAttribute('href', 'https://github.com/VaticanMemes');
+    node_3.appendChild(node_4);
+    
+    var node_5 = document.createTextNode((new String("Made by Vatican Memes")));
+    node_4.appendChild(node_5);
+
+    return node_2;
+}
+
+function loadHome() {
+    var main = document.getElementById("main");
+    main.textContent = "";
+    main.appendChild(createHome());
+}
+
+function createHome() {
+    return createParagrah("Home");
+}
+
+function loadCreate () {
+    var main = document.getElementById("main");
+    main.textContent = "";
+    main.appendChild(createCreate());
+}
+
+function createCreate () {
+    return createParagrah("Create")
+}
+
+function setActiveButton(button) {
+  const buttons = document.querySelectorAll(".button-nav");
+
+  buttons.forEach((button) => {
+    if (button !== this) {
+      button.classList.remove("active");
+    }
+  });
+
+  button.classList.add("active");
 }
 
 function createParagrah(text) {
     paragraph = document.createElement("p");
     paragraph.innerText = text;
-    return paragraph
+
+    return paragraph;
 }
 
 /*
@@ -233,6 +186,14 @@ function createParagrah(text) {
         <button class="btn btn-outline-success" type="submit">Search</button>
       </form>
     </div>
+  </div>
+</nav>
+*/
+
+/*
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-md">
+    <p>Made by <a href="https://github.com/VaticanMemes">Vatican Memes</a></p>
   </div>
 </nav>
 */
